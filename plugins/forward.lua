11298de16feaf15bd035e0cf9ab56df72f8413f5
naserteam <janjo.urmia2@gmail.com>
2016-03-07 02:23:21 +0330

    Update forward.lua

--- plugins/forward.lua
+++ plugins/forward.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-      fwd_msg('chat#id'..msg.to.id,'261809',ok_cb,false)
+      fwd_msg('chat#id'..msg.to.id,'46395',ok_cb,false)
 end 
 return {
   description = "Invite bot into a group chat", 
